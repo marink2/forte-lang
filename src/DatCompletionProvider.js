6446e7d8d80c837ae1a31fbc7a68bf7502f3a1ed
marink2 <kmarin3@emory.edu>
2024-12-30 02:33:38 -0500

    added scope to completion provider

--- src/DatCompletionProvider.js
+++ src/DatCompletionProvider.js
@@ -3,6 +3,34 @@ const getKeywordsFromYaml = require('./getKeywordsFromYaml');
 
 class DatCompletionProvider {
     provideCompletionItems(document, position, token, context) {
+        // Don't trigger completions if outside `set forte { ... }` block
+        const braceContentPattern = /(set\s+forte\s*\{[^}]*\})/gmi;
+        const documentText = document.getText();
+
+        let matchFound = false;
+        let match;
+        while ((match = braceContentPattern.exec(documentText)) !== null) {
+            const start = document.positionAt(match.index);
+            const end = document.positionAt(match.index + match[0].length);
+
+            if (position.isAfterOrEqual(start) && position.isBeforeOrEqual(end)) {
+                matchFound = true;
+                break;
+            }
+        }
+
+        if (!matchFound) {
+            return undefined;
+        }
+
+        // Don't trigger completions if a complete word exists prior to the cursor
+        const linePrefix = document.lineAt(position).text.substr(0, position.character);
+        const numPrefixWords = linePrefix.trim().split(/\s+/).length;
+
+        if (numPrefixWords > 1) {
+            return undefined;
+        }
+
         const keywords = getKeywordsFromYaml();
 
         // Convert keywords into completion items
